afcc5c9afee6cd6b9edd580d4602125d6c0145c4
Alvaro Ramirez <bosongluino@gmail.com>
2022-01-01 17:20:04 -0500

    creacion de un objeto
    uso de la clase Scanner.

--- src/examples/Areas.java
+++ src/examples/Areas.java
@@ -5,6 +5,9 @@ public class Areas {
 
 	public static void main(String[] args) {
 		
+/*acontinuacion se muestra el uso de la clase scanner, notese como 
+debemos crear un objeto*/
+		
 		System.out.println("introduce tu edad, por favor");
 		Scanner entry = new Scanner (System.in);
 		int edad = entry.nextInt();
@@ -25,6 +28,7 @@ public class Areas {
 		System.out.println("psicologo");	
 		}
 	}
-
+/* si se utiiza if ... ingresa al correspondiente fragmento de código y no 
+  ingresa a las demás opbiones*/
 }
 
